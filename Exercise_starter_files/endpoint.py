159c922337a816e22a5911285f61ef767056ddd6
abhijit-kalita <abhijit-kalita@users.noreply.github.com>
2022-01-03 04:02:37 +0530

    modified

--- Exercise_starter_files/endpoint.py
+++ Exercise_starter_files/endpoint.py
@@ -3,41 +3,101 @@ import json
 
 # URL for the web service, should be similar to:
 # 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
-scoring_uri = ""
-
+scoring_uri = 'http://86a273cd-5da3-44dc-9931-7876530b63bb.southcentralus.azurecontainer.io/score'
 # If the service is authenticated, set the key or token
-key = ""
+key = 'qIfCzIh9QtnO8BGj7LrFi3HsbEJJpweo'
 
 # Two sets of data to score, so we get two results back
-data = {
-    "data": [
-        {
-            "instant": 1,
-            "date": "2013-01-01 00:00:00,000000",
-            "season": 1,
-            "yr": 0,
-            "mnth": 1,
-            "weekday": 6,
-            "weathersit": 2,
-            "temp": 0.344167,
-            "atemp": 0.363625,
-            "hum": 0.805833,
-            "windspeed": 0.160446,
-            "casual": 331,
-            "registered": 654,
-        },
-    ]
-}
+data = {"data":
+        [
+          {
+"age": 17,
+      "job": "blue-collar",
+      "marital": "married",
+      "education": "university.degree",
+      "default": "no",
+      "housing": "yes",
+      "loan": "yes",
+      "contact": "cellular",
+      "month": "may",
+      "day_of_week": "mon",
+      "duration": 971,
+      "campaign": 1,
+      "pdays": 999,
+      "previous": 1,
+      "poutcome": "failure",
+      "emp.var.rate": 0,
+      "cons.price.idx": 0,
+      "cons.conf.idx": 0,
+      "euribor3m": 0,
+      "nr.employed": 0
+          },
+ {
+      "age": 0,
+      "job": "example_value",
+      "marital": "example_value",
+      "education": "example_value",
+      "default": "example_value",
+      "housing": "example_value",
+      "loan": "example_value",
+      "contact": "example_value",
+      "month": "example_value",
+      "day_of_week": "example_value",
+      "duration": 0,
+      "campaign": 0,
+      "pdays": 0,
+      "previous": 0,
+      "poutcome": "example_value",
+      "emp.var.rate": 0,
+      "cons.price.idx": 0,
+      "cons.conf.idx": 0,
+      "euribor3m": 0,
+      "nr.employed": 0
+          },
+      ]
+    }
 # Convert to JSON string
 input_data = json.dumps(data)
 with open("data.json", "w") as _f:
     _f.write(input_data)
 
 # Set the content type
-headers = {"Content-Type": "application/json"}
+headers = {'Content-Type': 'application/json'}
 # If authentication is enabled, set the authorization header
-headers["Authorization"] = f"Bearer {key}"
+headers['Authorization'] = f'Bearer {key}'
 
 # Make the request and display the response
 resp = requests.post(scoring_uri, input_data, headers=headers)
 print(resp.json())
+
+
+
+
+{
+  "data": [
+    {
+      "age": 0,
+      "job": "example_value",
+      "marital": "example_value",
+      "education": "example_value",
+      "default": "example_value",
+      "housing": "example_value",
+      "loan": "example_value",
+      "contact": "example_value",
+      "month": "example_value",
+      "day_of_week": "example_value",
+      "duration": 0,
+      "campaign": 0,
+      "pdays": 0,
+      "previous": 0,
+      "poutcome": "example_value",
+      "emp.var.rate": 0,
+      "cons.price.idx": 0,
+      "cons.conf.idx": 0,
+      "euribor3m": 0,
+      "nr.employed": 0
+    }
+  ],
+  "method": "predict"
+}
+
